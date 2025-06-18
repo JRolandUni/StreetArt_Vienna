4f5d93e915231cdb8f2b1b7485d58d7c02796da3
JRolandUni <a11920241@unet.univie.ac.at>
2025-06-18 18:40:08 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -25,12 +25,12 @@ L.marker(viennaCoords)
     // For each row, columns `Latitude`, `Longitude`, and `Title` are required
     for (var i in data) {
       var row = data[i];
-      console.log(row.lat)  
-      var marker = L.marker([row.lat, row.lon], {
-        opacity: 1
-      }).bindPopup(row.Title);
+      console.log(row.lat);  
+      //var marker = L.marker([row.lat, row.lon], {
+        //opacity: 1
+      //}).bindPopup(row.Title);
       
-      marker.addTo(map);
+      //marker.addTo(map);
     }
 
   });
